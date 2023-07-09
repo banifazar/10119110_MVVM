a78956d8af6369ad81d8a3dee01c60061f1ce4e3
Bani Faza Rasyadan <banifr@Banis-MacBook-Pro.local>
2023-07-09 21:40:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10119110_mvvm/MainViewModel.java
@@ -0,0 +1,31 @@
+package com.example.a10119110_mvvm;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    private final MutableLiveData<Meter> meter;
+
+    public MainViewModel() {
+        meter = new MutableLiveData<>();
+    }
+
+    public LiveData<Meter> getMeter(){
+        return meter;
+    }
+
+    public void calculateLength(String stringMeter) {
+        if (stringMeter.isEmpty())
+            stringMeter = "0";
+
+
+        double parsedMeter = Double.parseDouble(stringMeter);
+        Meter meter = new Meter();
+
+        meter.setMeter(parsedMeter);
+
+        this.meter.setValue(meter);
+
+    }
+}
